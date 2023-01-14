45a960740fa0826a0afe85c688f049341e0a1c9d
RodrigoLee56 <sousarodrigo56@outlook.com>
2023-01-13 22:20:53 -0400

    1003 - Soma Simples

---
+++ beecrowd/src/beecrowd/iniciante/SomaSimples_1003.java
@@ -0,0 +1,16 @@
+package beecrowd.iniciante;
+
+import java.util.Scanner;
+
+public class SomaSimples_1003 {
+
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+		int a = read.nextInt();
+		int b = read.nextInt();
+
+		System.out.println("SOMA = " + (a + b));
+		read.close();
+	}
+
+}
