f002b6d1cee9e94beea3e175a48e03c908d12dcf
RodrigoLee56 <sousarodrigo56@outlook.com>
2023-01-14 11:05:27 -0400

    1012 - Área

---
+++ beecrowd/src/beecrowd/iniciante/Area.java
@@ -0,0 +1,30 @@
+package beecrowd.iniciante;
+
+import java.util.Scanner;
+
+public class Area {
+
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+		double triangle, circle, trapezio, square, rectangle, a, b, c;
+		double pi = 3.14159;
+		a = read.nextDouble();
+		b = read.nextDouble();
+		c = read.nextDouble();
+
+		triangle = (a * c) / 2.0;
+		circle = pi * Math.pow(c, 2);
+		trapezio = ((a + b) * c) / 2.0;
+		square = Math.pow(b, 2);
+		rectangle = a * b;
+
+		System.out.printf("TRIANGULO: %.3f\n", triangle);
+		System.out.printf("CIRCULO: %.3f\n", circle);
+		System.out.printf("TRAPEZIO: %.3f\n", trapezio);
+		System.out.printf("QUADRADO: %.3f\n", square);
+		System.out.printf("RETANGULO: %.3f\n", rectangle);
+		
+		read.close();
+	}
+
+}
