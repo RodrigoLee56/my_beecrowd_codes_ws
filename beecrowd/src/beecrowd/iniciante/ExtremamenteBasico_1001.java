07cc2f6516c9b314e9d2dc7952b665aea2d243e9
RodrigoLee56 <sousarodrigo56@outlook.com>
2023-01-13 22:15:26 -0400

    1001 - Extremamente Básico

---
+++ beecrowd/src/beecrowd/iniciante/ExtremamenteBasico_1001.java
@@ -0,0 +1,16 @@
+package beecrowd.iniciante;
+
+import java.util.Scanner;
+
+public class ExtremamenteBasico_1001 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int value1 = s.nextInt();
+		int value2 = s.nextInt();
+		int X = value1 + value2;
+		System.out.println("X = " + X);
+		s.close();
+	}
+
+}
