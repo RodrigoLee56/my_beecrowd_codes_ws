5ac292ace867edbaba14164ce58b14de0a705946
RodrigoLee56 <sousarodrigo56@outlook.com>
2023-01-13 22:58:18 -0400

    1009 - Salário com Bônus

---
+++ beecrowd/src/beecrowd/iniciante/SalarioComBonus.java
@@ -0,0 +1,20 @@
+package beecrowd.iniciante;
+
+import java.util.Scanner;
+
+public class SalarioComBonus {
+
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+        String nome = read.next();
+        double fixedSalary = read.nextDouble();
+        double sold = read.nextDouble();
+        
+        double total = (sold * 0.15) + fixedSalary;
+        
+        System.out.printf("TOTAL = R$ %.2f\n", total);
+        
+        read.close();
+	}
+
+}
